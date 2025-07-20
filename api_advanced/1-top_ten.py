ce3ea2a1f3ce31a875ad3494849a13062ad42e87
Enock <e.ntwari@alustudent.com>
2025-07-20 19:23:20 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 10 hot posts
-for a given subreddit.
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
+
 def top_ten(subreddit):
-    " Prints the titles of the first 10 hot posts for a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Mozilla/5.0 (compatible; RedditAPIbot/1.0)"}
-    params = {"limit": 10}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-
-        for post in posts:
-            print(post.get("data", {}).get("title"))
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
